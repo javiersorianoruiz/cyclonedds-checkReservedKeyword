20b86dc085829a35ba1e3d44c7586604c6c9e620
javiersorianoruiz <soriano_ruiz_javier@hotmail.com>
2022-10-18 07:56:51 +0000

    test complete coverage

--- test/test.cpp
+++ test/test.cpp
@@ -18,3 +18,7 @@ TEST(checkReservedKeyword_TS, ok_param_equal_0) {
 TEST(checkReservedKeyword_TS, _param_equal_1) {
     ASSERT_EQ(foo(1), -1);  
 }
+
+TEST(checkReservedKeyword_TS, _param_equal_1_with_minus_1) {
+    ASSERT_EQ(foo(-1), -1);  
+}
